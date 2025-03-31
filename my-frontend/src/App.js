ce114c83a8ba170038ce53f1528b00e33779f447
shawla4real <65089196+shawla4real@users.noreply.github.com>
2025-03-31 16:34:29 -0400

    Update App.js

--- my-frontend/src/App.js
+++ my-frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    // If running locally, ensure the URL matches your Express server's URL/port
+    fetch('http://54.237.134.142:8080')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => console.error('Error fetching API:', err));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{message || 'Loading...'}</h1>
     </div>
   );
 }
