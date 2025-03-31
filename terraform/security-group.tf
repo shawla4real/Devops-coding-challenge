c4102c9b026c37e5a722128f245d3615ab466138
Sola <sola.onabolu@gmail.com>
2025-03-31 15:19:50 -0400

    update sg.tf

---
+++ terraform/security-group.tf
@@ -0,0 +1,41 @@
+
+resource "aws_security_group" "app" {
+  name        = "Jenkins_sg"
+  description = "Jenkins security group"
+  vpc_id      = data.aws_vpc.vpc.id
+
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # we can limit ip here
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.common_tags
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
